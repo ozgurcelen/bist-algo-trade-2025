8391462ef41f164bd85b4e3e32ecb192f7978448
ozgurcelen <44448439+ozgurcelen@users.noreply.github.com>
2024-12-11 01:22:42 +0300

    Update create_table.py

--- pages/create_table.py
+++ pages/create_table.py
@@ -7,31 +7,6 @@ import warnings
 from urllib import request
 import ssl
 
-# Veritabanı oluşturma fonksiyonu
-def create_database():
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            age INTEGER,
-            email TEXT
-        )
-    ''')
-    conn.commit()
-    cursor.executemany('''
-        INSERT INTO users (name, age, email)
-        VALUES (?, ?, ?)
-    ''', [
-        ("Ali", 30, "ali@example.com"),
-        ("Ayşe", 25, "ayse@example.com"),
-        ("Veli", 35, "veli@example.com")
-    ])
-    conn.commit()
-    conn.close()
-    st.success("Veritabanı ve tablo başarıyla oluşturuldu!")
-
 # Veritabanından veri çekme fonksiyonu
 def fetch_data():
     conn = sqlite3.connect('db.sqlite3')
@@ -49,18 +24,10 @@ def fetch_data_2():
 # Streamlit Arayüzü
 st.title("Veritabanı İşlemleri")
 
-# Veritabanı oluşturma butonu
-if st.button("Veritabanı Oluştur"):
-    create_database()
-
 # Veritabanından veri çekme butonu
 if st.button("Verileri Göster"):
     data = fetch_data_2()
-    if data:
-        st.write("Tablodaki Veriler:")
-        st.table(data)  # Veriyi tablo olarak gösterir
-    else:
-        st.warning("Veritabanında herhangi bir veri yok!")
+    st.table(data)  # Veriyi tablo olarak gösterir
 
 
 # Ayarlar
