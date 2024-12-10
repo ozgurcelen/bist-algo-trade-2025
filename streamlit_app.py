f210331f013c2a323fe9a948df24240fc289081f
ozgurcelen <ozgurrcelen@gmail.com>
2024-12-10 23:59:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("🎈 Bist Algo Trade Robot")
 st.write(
-    "Bist Algo Trade Robotik İşlemler. Burada tamamen eğitim amaçlı makine öğrenmesi yöntemi ile hisse senetlerinin trendi takip edilmektedir. Lütfen buradaki bilgilere göre alım satım yapmayanız"
+    "Bist Algo Trade Robotik İşlemler. Burada tamamen eğitim amaçlı makine öğrenmesi yöntemi ile hisse senetlerinin trendi takip edilmektedir. "
+    "Lütfen buradaki bilgilere göre alım satım yapmayanız"
 )
