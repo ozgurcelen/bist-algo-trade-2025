e199f2b720541192e191400ecec719299d354f81
ozgurcelen <44448439+ozgurcelen@users.noreply.github.com>
2024-12-10 21:06:56 +0000

    update edildi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("BIST Algo Trade Robotik İşlemler")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Bist Algo Trade Merhaba Der"
 )
